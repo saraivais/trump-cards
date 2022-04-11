2f1502fa7af2076b4bd1a185b58f4ddc3950613a
saraivais <isadorasaraiva95@gmail.com>
2022-04-11 16:56:06 -0300

    Feat: Card on App~ Card component imported and called on App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Form from './components/Form';
+import Card from './components/Card';
 // Iniciando projeto~
 
 class App extends React.Component {
@@ -8,6 +9,7 @@ class App extends React.Component {
       <div>
         <h1>Tryunfo</h1>
         <Form />
+        <Card />
       </div>
     );
   }
