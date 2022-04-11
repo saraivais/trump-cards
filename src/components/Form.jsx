88c96a6a6b50419fe220e70330734590981785be
saraivais <isadorasaraiva95@gmail.com>
2022-04-11 16:08:24 -0300

    Feat: Form props & propTypes~ new props added, validated w propTypes, props added to input components

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,25 +1,96 @@
 import React from 'react';
+import propTypes from 'prop-types';
 import GenericImput from './GenericImput';
+// hasTrunfo, hasTrunfo: propTypes.bool.isRequired,
 
 class Form extends React.Component {
   render() {
+    const {
+      cardName,
+      cardDescription,
+      cardAttr1,
+      cardAttr2,
+      cardAttr3,
+      cardImage,
+      cardRare,
+      cardTrunfo,
+      isSaveButtonDisabled,
+      onInputChange,
+      onSaveButtonClick,
+    } = this.props;
     return (
       <>
-        <GenericImput inputType="text" testId="name-input" />
-        <GenericImput inputType="textarea" testId="description-input" />
-        <GenericImput inputType="number" testId="attr1-input" />
-        <GenericImput inputType="number" testId="attr2-input" />
-        <GenericImput inputType="number" testId="attr3-input" />
-        <GenericImput inputType="text" testId="image-input" />
-        <select data-testid="rare-input">
+        <GenericImput
+          valueShown={ cardName }
+          changeFunc={ onInputChange }
+          inputType="text"
+          testId="name-input"
+        />
+        <GenericImput
+          valueShown={ cardDescription }
+          changeFunc={ onInputChange }
+          inputType="textarea"
+          testId="description-input"
+        />
+        <GenericImput
+          valueShown={ cardAttr1 }
+          changeFunc={ onInputChange }
+          inputType="number"
+          testId="attr1-input"
+        />
+        <GenericImput
+          valueShown={ cardAttr2 }
+          changeFunc={ onInputChange }
+          inputType="number"
+          testId="attr2-input"
+        />
+        <GenericImput
+          valueShown={ cardAttr3 }
+          changeFunc={ onInputChange }
+          inputType="number"
+          testId="attr3-input"
+        />
+        <GenericImput
+          valueShown={ cardImage }
+          changeFunc={ onInputChange }
+          inputType="text"
+          testId="image-input"
+        />
+        <select data-testid="rare-input" value={ cardRare } onChange={ onInputChange }>
           <option value="normal">Normal</option>
           <option value="raro">Raro</option>
           <option value="muito raro">Muito Raro</option>
         </select>
-        <GenericImput inputType="checkbox" testId="trunfo-input" />
-        <button type="button" data-testid="save-button">Salvar</button>
+        <input
+          type="checkbox"
+          data-testid="trunfo-input"
+          checked={ cardTrunfo }
+          onClick={ onInputChange }
+        />
+        <button
+          disabled={ isSaveButtonDisabled }
+          onClick={ onSaveButtonClick }
+          type="button"
+          data-testid="save-button"
+        >
+          Salvar
+        </button>
       </>);
   }
 }
+
+Form.propTypes = {
+  cardName: propTypes.string.isRequired,
+  cardDescription: propTypes.string.isRequired,
+  cardAttr1: propTypes.string.isRequired,
+  cardAttr2: propTypes.string.isRequired,
+  cardAttr3: propTypes.string.isRequired,
+  cardImage: propTypes.string.isRequired,
+  cardRare: propTypes.string.isRequired,
+  cardTrunfo: propTypes.bool.isRequired,
+  isSaveButtonDisabled: propTypes.bool.isRequired,
+  onInputChange: propTypes.func.isRequired,
+  onSaveButtonClick: propTypes.func.isRequired,
+};
 
 export default Form;
