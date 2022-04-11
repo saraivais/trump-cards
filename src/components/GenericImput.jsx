25610c9d8382103fbb99702e3ed5d472a1b54a1f
saraivais <isadorasaraiva95@gmail.com>
2022-04-11 17:52:30 -0300

    Feat: GenericInput changes~ Now adds 'name' for each input, to be of use in the onInputChange function~

--- src/components/GenericImput.jsx
+++ src/components/GenericImput.jsx
@@ -3,8 +3,9 @@ import propTypes from 'prop-types';
 
 class GenericImput extends React.Component {
   render() {
-    const { inputType, testId, valueShown, changeFunc } = this.props;
+    const { inputName, inputType, testId, valueShown, changeFunc } = this.props;
     return (<input
+      name={ inputName }
       type={ inputType }
       value={ valueShown }
       onChange={ changeFunc }
@@ -14,6 +15,7 @@ class GenericImput extends React.Component {
 }
 
 GenericImput.propTypes = {
+  inputName: propTypes.string.isRequired,
   inputType: propTypes.string.isRequired,
   testId: propTypes.string.isRequired,
   valueShown: propTypes.string.isRequired,
