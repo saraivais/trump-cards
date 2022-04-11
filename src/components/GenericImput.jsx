da1d1a136ee6f002fe4817ae11301625f9169d0f
saraivais <isadorasaraiva95@gmail.com>
2022-04-11 16:04:36 -0300

    Feat: GenericImput modified~ Now receives as props onChange and value~ (mais generico ainda, o retorno)

--- src/components/GenericImput.jsx
+++ src/components/GenericImput.jsx
@@ -3,14 +3,21 @@ import propTypes from 'prop-types';
 
 class GenericImput extends React.Component {
   render() {
-    const { inputType, testId } = this.props;
-    return <input type={ inputType } data-testid={ testId } />;
+    const { inputType, testId, valueShown, changeFunc } = this.props;
+    return (<input
+      type={ inputType }
+      value={ valueShown }
+      onChange={ changeFunc }
+      data-testid={ testId }
+    />);
   }
 }
 
 GenericImput.propTypes = {
   inputType: propTypes.string.isRequired,
   testId: propTypes.string.isRequired,
+  valueShown: propTypes.string.isRequired,
+  changeFunc: propTypes.func.isRequired,
 };
 
 export default GenericImput;
