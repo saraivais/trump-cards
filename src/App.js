edf8229e5a87692bd33bc59ad64be10b8738b28d
saraivais <isadorasaraiva95@gmail.com>
2022-04-11 17:54:40 -0300

    Feat: state & method created~ Added state matching w input names, created 2 functions, one serious, another placeholder for input changes~ controlled components

--- src/App.js
+++ src/App.js
@@ -4,12 +4,62 @@ import Card from './components/Card';
 // Iniciando projeto~
 
 class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      Name: '',
+      Description: '',
+      Attr1: '',
+      Attr2: '',
+      Attr3: '',
+      Image: '',
+      Rarity: '',
+      Trunfo: '',
+    };
+
+    this.onInputChange = this.onInputChange.bind(this);
+  }
+
+  onInputChange({ target }) {
+    const { name } = target;
+    const value = (target.type === 'checkbox') ? target.checked : target.value;
+    this.setState({
+      [name]: value,
+    });
+  }
+
+  xablau() {
+    console.log('dhaudhs');
+    return 2;
+  }
+
   render() {
+    const { Name, Description, Attr1, Attr2, Attr3, Image, Rarity, Trunfo } = this.state;
     return (
       <div>
         <h1>Tryunfo</h1>
-        <Form />
-        <Card />
+        <Form
+          cardName={ Name }
+          cardDescription={ Description }
+          cardAttr1={ Attr1 }
+          cardAttr2={ Attr2 }
+          cardAttr3={ Attr3 }
+          cardImage={ Image }
+          cardRare={ Rarity }
+          cardTrunfo={ Trunfo }
+          onInputChange={ this.onInputChange }
+          onSaveButtonClick={ this.xablau }
+        />
+        <Card
+          cardName={ Name }
+          cardDescription={ Description }
+          cardAttr1={ Attr1 }
+          cardAttr2={ Attr2 }
+          cardAttr3={ Attr3 }
+          cardImage={ Image }
+          cardRare={ Rarity }
+          cardTrunfo={ Trunfo }
+        />
       </div>
     );
   }
