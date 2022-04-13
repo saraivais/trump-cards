628758eae6954253960ec90668e5d4b79863d948
saraivais <isadorasaraiva95@gmail.com>
2022-04-13 11:45:03 -0300

    Style: correct name~ imput... 🤦 dishonor on me, dishonor on my cow

---
+++ src/components/GenericInput.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import propTypes from 'prop-types';
+
+class GenericInput extends React.Component {
+  render() {
+    const {
+      inputName,
+      inputType,
+      testId,
+      valueShown,
+      changeFunc,
+      labelText,
+    } = this.props;
+    return (
+      <label htmlFor={ inputName }>
+        { labelText }
+        <input
+          name={ inputName }
+          type={ inputType }
+          value={ valueShown }
+          onChange={ changeFunc }
+          data-testid={ testId }
+        />
+      </label>);
+  }
+}
+
+GenericInput.propTypes = {
+  inputName: propTypes.string.isRequired,
+  inputType: propTypes.string.isRequired,
+  testId: propTypes.string.isRequired,
+  valueShown: propTypes.string.isRequired,
+  changeFunc: propTypes.func.isRequired,
+  labelText: propTypes.string.isRequired,
+};
+
+export default GenericInput;
